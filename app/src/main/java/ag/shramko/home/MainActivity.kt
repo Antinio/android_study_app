620e17170cd534f0c9ff8389e10bebdc7625fd57
anton <anton.shramko@youcontrol.com.ua>
2019-12-05 22:53:35 +0200

    Added API request on news services and showing news titles on the main view.

--- app/src/main/java/ag/shramko/home/MainActivity.kt
+++ app/src/main/java/ag/shramko/home/MainActivity.kt
@@ -1,52 +1,53 @@
 package ag.shramko.home
 
 import android.content.Intent
-import android.os.AsyncTask
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import com.google.gson.Gson
-import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
-import java.lang.RuntimeException
-import java.net.HttpURLConnection
-import java.net.URL
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var vText:TextView
-    var request:Disposable?=null
+    lateinit var vText: TextView
+    lateinit var vList: LinearLayout
+    var request: Disposable? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        vText = findViewById<TextView>(R.id.act1_text)
-        vText.setTextColor(0xFFFF0000.toInt())
-        vText.setOnClickListener {
-            Log.e("tag", "Нажата")
-//            val i = Intent(this, SecondActivity::class.java)
-//            i.putExtra("tag1", vText.text)
-//            startActivityForResult(i, 0)
+        vList = findViewById<LinearLayout>(R.id.act1_list)
 
-            val o = createRequest("https://api.rss2json.com/v1/api.json?rss_url=http%3A%2F%2Ffeeds.bbci.co.uk%2Fnews%2Frss.xml")
+        val o =
+            createRequest("https://api.rss2json.com/v1/api.json?rss_url=http%3A%2F%2Ffeeds.bbci.co.uk%2Fnews%2Frss.xml")
                 .map { Gson().fromJson(it, Feed::class.java) }
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
 
-            request = o.subscribe({
+        request = o.subscribe({
 
-                for (item in it.items)
-                    Log.w("test", "title: ${item.title}")
-            },{
-                Log.e("test","", it)
-            })
+            showLinerLayout(it.items)
+            //                for (item in it.items)
+//                    Log.w("test", "title: ${item.title}")
+        }, {
+            Log.e("test", "", it)
+        })
+    }
+
+    fun showLinerLayout(feedList: ArrayList<FeedItem>) {
+
+        val inflater = layoutInflater
+        for (f in feedList) {
+            val view = inflater.inflate(R.layout.list_item, vList, false)
+            val vTitle = view.findViewById<TextView>(R.id.item_title)
+            vTitle.text = f.title
+            vList.addView(view)
         }
-
-        Log.v("tag", "Был запущен onCreate")
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -55,7 +56,7 @@ class MainActivity : AppCompatActivity() {
         if (data != null) {
             val str = data.getStringExtra("tag2")
 
-            vText.text=str
+            vText.text = str
         }
     }
 
@@ -82,12 +83,12 @@ class MainActivity : AppCompatActivity() {
 }
 
 class Feed(
-    val items:ArrayList<FeedItem>
+    val items: ArrayList<FeedItem>
 )
 
 class FeedItem(
-    val title:String,
-    val link:String,
-    val thumbnail:String,
-    val description:String
+    val title: String,
+    val link: String,
+    val thumbnail: String,
+    val description: String
 )
