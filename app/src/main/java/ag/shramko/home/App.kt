1b255d3b21114fdaa61d687e124e3c87b7805773
anton <shramko.ag@gmail.com>
2019-12-15 19:48:31 +0200

    Fix code formatting

--- app/src/main/java/ag/shramko/home/App.kt
+++ app/src/main/java/ag/shramko/home/App.kt
@@ -4,7 +4,7 @@ import android.app.Application
 import io.realm.Realm
 import io.realm.RealmConfiguration
 
-class App: Application() {
+class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
